ba63ede82b3714ecdfeb6b64581984bd007c49a3
Jon Holman <jon.holman@armedia.com>
2021-07-20 14:10:51 -0400

    Update get-artifacts.sh

--- get-artifacts.sh
+++ get-artifacts.sh
@@ -5,6 +5,6 @@ cd "$here"
 cloud_config_version="2021.03"
 rm -rf artifacts
 mkdir artifacts
-echo "Downloading  config version $cloud_config_version"
+echo "Downloading config version $cloud_config_version"
 aws s3 cp "s3://arkcase-container-artifacts/ark_cloudconfig/artifacts/config-server-${cloud_config_version}-SNAPSHOT.jar" artifacts/
 aws s3 cp "s3://arkcase-container-artifacts/ark_cloudconfig/artifacts/start.sh" artifacts/
